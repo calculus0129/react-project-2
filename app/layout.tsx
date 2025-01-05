e6c96b5c2bff5bc0cb1ce575a40d2a15498961ea
calculus0129 <ryanbhang129@naver.com>
2025-01-05 18:15:39 +0900

    feat: style only via the page.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+// import "./globals.css";
 
 export const metadata: Metadata = {
   title: "React Tic-Tac-Toe",
