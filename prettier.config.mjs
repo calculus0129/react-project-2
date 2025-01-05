f1a4b28955e1bbf6f0949c9ef3f03a2669bb8f17
calculus0129 <ryanbhang129@naver.com>
2025-01-05 17:06:19 +0900

    refactor: change code prettier style

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -2,7 +2,7 @@ const prettierConfig = {
   semi: true, // Add semicolons at the end of statements
   singleQuote: false, // Use double quotes
   tabWidth: 2, // Indentation is 2 spaces
-  trailingComma: "es5", // Add trailing commas in ES5 syntax (objects, arrays, etc.)
+  trailingComma: "all", // Include trailing commas wherever possible (alt: es5)
   printWidth: 80, // Maximum line width is 80 characters
   useTabs: false, // Use spaces instead of tabs for indentation
   bracketSpacing: true, // Add spaces inside object literal brackets
