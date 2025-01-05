017527a959e1e9987aada9bcfb434cb7362a0d73
calculus0129 <ryanbhang129@naver.com>
2025-01-05 18:15:20 +0900

    feat: add components and elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
 import "./page.css";
 
 export default function Home() {
-  return <p>Coming Soon...</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
+  );
 }
